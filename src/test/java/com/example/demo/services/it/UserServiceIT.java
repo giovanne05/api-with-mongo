8b619cc8d183ccb58b7b619263d80a027cde2c98
giovannesantos <giovanne.dasilvasantos-ext@ab-inbev.com>
2023-11-14 17:02:32 -0300

    test: test container UserServiceIT

--- src/test/java/com/example/demo/services/it/UserServiceIT.java
+++ src/test/java/com/example/demo/services/it/UserServiceIT.java
@@ -1,5 +1,46 @@
 package com.example.demo.services.it;
 
-@TestContainer()
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers()
+@SpringBootTest
 public class UserServiceIT {
+
+    @Autowired
+    UserService userService;
+
+    static String ID = "1234";
+
+    static String NAME = "name";
+
+    static int AGE = 18;
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.6");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    public void shouldSaveUser() {
+        User user = new User(ID, NAME, AGE);
+
+        userService.saveUser(user);
+        User userById = userService.getUserById(ID);
+
+        Assertions.assertEquals(userById, user);
+    }
 }
