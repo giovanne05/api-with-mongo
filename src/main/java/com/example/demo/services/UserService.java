2cbd5059d97be6840d3b757c025ca08a6c5ef5d8
giovannesantos <giovanne.dasilvasantos-ext@ab-inbev.com>
2023-11-14 22:08:20 -0300

    test: more tests to UserServiceIT

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -34,4 +34,8 @@ public class UserService {
     public void deleteUserById(String id) {
         userRepository.deleteById(id);
     }
+
+    public void deleteAll() {
+        userRepository.deleteAll();
+    }
 }
--- src/test/java/com/example/demo/services/it/UserServiceIT.java
+++ src/test/java/com/example/demo/services/it/UserServiceIT.java
@@ -2,9 +2,9 @@ package com.example.demo.services.it;
 
 
 import com.example.demo.entities.User;
+import com.example.demo.exceptions.UserNotFoundException;
 import com.example.demo.services.UserService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -12,6 +12,8 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
 
 @Testcontainers()
 @SpringBootTest
@@ -34,13 +36,60 @@ public class UserServiceIT {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
 
+    @AfterEach
+    void tearDown() {
+        userService.deleteAll();
+    }
+
     @Test
     public void shouldSaveUser() {
         User user = new User(ID, NAME, AGE);
 
         userService.saveUser(user);
-        User userById = userService.getUserById(ID);
-
-        Assertions.assertEquals(userById, user);
+        Assertions.assertEquals(userService.getUserById(ID), user);
     }
+
+    @Test
+    public void shouldReturnAllUser() {
+        User first_user = new User(ID, NAME, AGE);
+        User second_user = new User("2345", NAME, AGE);
+
+        List<User> users = List.of(first_user, second_user);
+
+        userService.saveUser(first_user);
+        userService.saveUser(second_user);
+
+        Assertions.assertEquals(userService.getAllUsers(), users);
+
+    }
+
+    @Test
+    public void shouldUpdateUser() {
+        User user = new User(ID, NAME, AGE);
+
+        userService.saveUser(user);
+
+        user.setName("updateUser");
+        user.setAge(39);
+
+        userService.saveUser(user);
+
+        Assertions.assertEquals(userService.getUserById(ID), user);
+
+    }
+
+    @Test
+    public void shouldDeleteUserById() {
+        User user = new User(ID, NAME, AGE);
+
+        userService.saveUser(user);
+
+        Assertions.assertEquals(userService.getAllUsers().size(), 1);
+
+        userService.deleteUserById(ID);
+
+        Assertions.assertEquals(userService.getAllUsers().size(), 0);
+
+    }
+
 }
